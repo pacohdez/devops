d70d79f7b5e5f77c9ed9e97bc17f4bb9ab68cf2f
pacohdez <ischernandezcfco@gmail.com>
2024-09-07 13:00:19 -0600

    DevOps

--- backend/src/main/java/mx/edu/itlapiedad/ws/Mensaje.java
+++ backend/src/main/java/mx/edu/itlapiedad/ws/Mensaje.java
@@ -24,7 +24,7 @@ public class Mensaje {
 	
 	@GetMapping("servicio")
 	public String enviarSaludo() {
-		return "Hola Github Actions desde Devops 2024";
+		return "Hola Github Actions desde Devops 2024 - Curso TMQ";
 	}
 	
 	@GetMapping("alumno")
